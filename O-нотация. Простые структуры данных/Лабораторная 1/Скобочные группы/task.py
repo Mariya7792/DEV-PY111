c2f8b7c0e81946139a7a53b26e1c89b2bbb9f9a5
Mariya7792 <sophi5@yandex.ru>
2024-06-06 13:39:36 +0300

    Лабораторная 1

--- O-нотация. Простые структуры данных/Лабораторная 1/Скобочные группы/task.py
+++ O-нотация. Простые структуры данных/Лабораторная 1/Скобочные группы/task.py
@@ -1,3 +1,4 @@
+from collections import deque
 def check_brackets(brackets_row: str) -> bool:
     """
     Проверьте, является ли входная строка допустимой последовательностью скобок
@@ -5,8 +6,19 @@ def check_brackets(brackets_row: str) -> bool:
     :param brackets_row: Входная строка для проверки
     :return: True, если последовательность корректна, False в противном случае
     """
+    list_brackets = []
     ...  # TODO реализовать проверку скобочной группы
-
+    try:
+        for element in brackets_row:
+            if element == "(":
+                list_brackets.append(element)
+            elif element == ")":
+                list_brackets.pop()
+        if list_brackets:
+            return False
+        return True
+    except IndexError:
+        return False
 
 if __name__ == '__main__':
     print(check_brackets("()()"))  # True
