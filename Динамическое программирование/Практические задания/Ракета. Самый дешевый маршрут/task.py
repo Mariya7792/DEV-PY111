efc7ed5b2416296f266f7e19ca44f6038f7a8507
marbur <sophi5@yandex.ru>
2024-06-20 21:17:44 +0300

    Практика 4

--- Динамическое программирование/Практические задания/Ракета. Самый дешевый маршрут/task.py
+++ Динамическое программирование/Практические задания/Ракета. Самый дешевый маршрут/task.py
@@ -1,3 +1,4 @@
+import pprint
 from typing import List
 
 
@@ -11,7 +12,25 @@ def rocket_coasts(table: List[List[int]]) -> List[List[int]]:
     :return: Таблицу стоимостей перемещения по клеткам
     """
     ...  # TODO рассчитать таблицу стоимостей перемещений
+    n = len(table)
+    m = len(table[0])
+    table_cost = [[0] * m for i in range(n)]
 
+    table_cost[0][0] = table[0][0]
+
+    for index in range(1, len(table[0])):
+        table_cost[0][index] = table_cost[0][index - 1] + table[0][index]
+
+    for index in range(1, len(table)):
+        table_cost[index][0] = table_cost[index - 1][0] + table[index][0]
+    pprint.pprint(table_cost, width=20)
+
+    for i in range(1, n):
+        for j in range(1, n):
+            table_cost[i][j] = min(table_cost[i - 1][j], table_cost[i][j - 1]) + table[i][j]
+
+        pprint.pprint(table_cost, width=20)
+    return table_cost
 
 if __name__ == '__main__':
     coasts_ceil = [
