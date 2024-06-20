82d183c9242efb93a0190cc4f1112c8dc9e03de1
Mariya7792 <sophi5@yandex.ru>
2024-06-20 12:13:32 +0300

    Лабораторная 3

--- Итеративные и рекурсивные алгоритмы/Лабораторная 2/Рекурсивный факториал/task.py
+++ Итеративные и рекурсивные алгоритмы/Лабораторная 2/Рекурсивный факториал/task.py
@@ -7,7 +7,9 @@ def factorial_recursive(n: int) -> int:
     """
     if n == 0:
         return 1
-    elif n >= 1:
+    elif n > 0:
         return n * factorial_recursive(n - 1)
+    elif type(n) == float:
+        raise TypeError
     raise ValueError
      # TODO реализовать рекурсивный алгоритм нахождения факториала
--- Сортировки/Лабораторная 3/Сортировка подсчетами/task.py
+++ Сортировки/Лабораторная 3/Сортировка подсчетами/task.py
@@ -12,17 +12,21 @@ def sort(container: Sequence[int]) -> Sequence[int]:
     :param container: Массив, который надо отсортировать
     :return: Отсортированный в порядке возрастания массив
     """
-    ...  # TODO реализовать алгоритм сортировки подсчетами
+    # TODO реализовать алгоритм сортировки подсчетами
     if not container:
         return container
+
     min_val = min(container)
     max_val = max(container)
-    data = {key: [] for key in range(min_val, max_val + 1)}
+    data = {key: [] for key in range(min_val, max_val+1)}
+
     sorted_result = []
     for value in container:
         data[value].append(value)
 
-    for key, value in data.values():
+    for value in data.values():
         if value:
             sorted_result.extend(value)
+
     return sorted_result
+
