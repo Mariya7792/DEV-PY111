f82548ac167a5343708bbf3107704f9ae44b11a2
marbur <sophi5@yandex.ru>
2024-06-27 21:14:33 +0300

    Практика 5

--- Графы и деревья/Практические задания/Обход в глубину. Рекурсивный способ/task.py
+++ Графы и деревья/Практические задания/Обход в глубину. Рекурсивный способ/task.py
@@ -1,5 +1,6 @@
 from typing import Hashable, List
 import networkx as nx
+from collections import deque
 
 
 def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
@@ -13,3 +14,41 @@ def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :return: Список узлов в порядке посещения.
     """
     ...  # TODO реализовать обход в глубину
+    visited = {node: False for node in g.nodes}
+    # d = deque()
+    path = []
+
+    def rec_dfs(current_node):
+        visited[current_node] = True
+        path.append(current_node)
+
+    # d.append(start_node)
+    # visited[start_node] = True
+
+    # while d:
+    #     current_node = d.pop()
+    #     path.append(current_node)
+
+        for neighbor in g.neighbors(current_node):
+            if not visited[neighbor]:
+                rec_dfs(neighbor)
+                # d.append(neighbor)
+                # visited[neighbor] = True
+    rec_dfs(start_node)
+    return path
+
+
+if __name__ == '__main__':
+    # TODO записать граф с помощью модуля networkx и прверить обход в ширину
+    graph = nx.Graph()
+    graph.add_nodes_from('ABCDEGGHIJ')
+    graph.add_edges_from([('A', 'B'),
+                          ('A', 'C'),
+                          ('B', 'D'),
+                          ('B', 'E'),
+                          ('E', 'G'),
+                          ('C', 'F')
+                          ])
+    print(dfs(graph, 'A'))
+    # nx.draw(graph, with_labels=True)
+    # plt.show()
--- Графы и деревья/Практические задания/Обход в ширину/task.py
+++ Графы и деревья/Практические задания/Обход в ширину/task.py
@@ -1,6 +1,6 @@
 from typing import Hashable, List
 from collections import deque
-
+import matplotlib.pyplot as plt
 import networkx as nx
 
 
@@ -15,7 +15,41 @@ def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :return: Список узлов в порядке посещения.
     """
     ...  # TODO реализовать обход в ширину
+    visited = {node: False for node in g.nodes}
+    d = deque()
+    path = []
 
+    d.append(start_node)
+    visited[start_node] = True
+
+    while d:
+        current_node = d.popleft()
+        path.append(current_node)
+
+        for neighbor in g.neighbors(current_node):
+            if not visited[neighbor]:
+                d.append(neighbor)
+                visited[neighbor] = True
+    return path
 
 if __name__ == '__main__':
     # TODO записать граф с помощью модуля networkx и прверить обход в ширину
+    graph = nx.Graph()
+    graph.add_nodes_from('ABCDEGGHIJ')
+    graph.add_edges_from([('A', 'B'),
+                          ('A', 'F'),
+                          ('B', 'G'),
+                          ('F', 'G'),
+                          ('G', 'C'),
+                          ('G', 'H'),
+                          ('G', 'I'),
+                          ('C', 'H'),
+                          ('H', 'I'),
+                          ('H', 'J'),
+                          ('H', 'E'),
+                          ('H', 'D'),
+                          ('E', 'D')
+                          ])
+    print(bfs(graph, 'A'))
+    # nx.draw(graph, with_labels=True)
+    # plt.show()
